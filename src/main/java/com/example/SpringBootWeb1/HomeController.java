72dcdeb8ba6ffa0de9d1154759731e4b6cf94525
shrutiPandey <shrutipandey23456@gmail.com>
2025-03-06 22:44:45 +0530

    adding ModelAndView object instead of Model object

--- src/main/java/com/example/SpringBootWeb1/HomeController.java
+++ src/main/java/com/example/SpringBootWeb1/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -18,12 +19,13 @@ public class HomeController {
 	}
 	
 	@RequestMapping("add")
-	public String add(@RequestParam("num1") int num, int num2, Model model) {		
+	public ModelAndView add(@RequestParam("num1") int num, int num2, ModelAndView mv) {		
 		
 		int sum = num+num2;
 		
-		model.addAttribute("sum", sum);
+		mv.addObject("sum", sum);
+		mv.setViewName("result");
 		
-		return "result";
+		return mv;
 	}
 }
